76490fbfce27c3b443b60f34702505b331e0c4e8
Ch Ammar <96951639+chammar55@users.noreply.github.com>
2023-02-03 19:37:06 +0500

    quizler_app

---
+++ android/app/src/main/kotlin/com/example/quizler_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.quizler_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ questions.dart
@@ -0,0 +1,8 @@
+class Questions {
+  late String questionText;
+  late bool questionAnswer;
+  Questions({required String q, required bool a}) {
+    questionText = q;
+    questionAnswer = a;
+  }
+}
